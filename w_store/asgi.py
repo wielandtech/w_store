bb61716fe6164d17aec06ba055e97ce10c67c56a
Raphael Wieland <raphael@wielandtech.com>
2025-04-22 01:02:52 -0400

    create w_store

---
+++ w_store/asgi.py
@@ -0,0 +1,17 @@
+"""
+ASGI config for w_store project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+import sys
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'w_store.settings')
+
+application = get_asgi_application()
---
+++ w_store/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for w_store project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'w_store.settings')
+
+application = get_wsgi_application()
